3e0e0a2beb09799fa2736c56f8277858abea3a3c
João Pedro Uchôa <joaopedrouchoacampos@gmail.com>
2024-01-24 19:09:48 -0300

    Componente de cursos e outras pequenas mudanças

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Courses } from '../model/courses';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+  courses: Courses[] = [
+    { _id: '1', name: 'Angular', category: 'front-end' }
+  ]
+
+  displayedColumns = ['name','category'];
 
 }
