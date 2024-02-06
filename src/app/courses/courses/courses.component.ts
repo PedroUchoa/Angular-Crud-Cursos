88472c82e632401018cfd39f89a84669c901ae9c
João Pedro Uchôa <joaopedrouchoacampos@gmail.com>
2024-02-05 21:45:51 -0300

    Add: component de editar é excluir cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 
 import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
@@ -11,28 +12,37 @@ import { CoursesService } from '../services/courses.service';
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent implements OnInit{
+export class CoursesComponent implements OnInit {
 
   courses$: Observable<Courses[]>;
 
-  displayedColumns = ['name','category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
-  constructor(private courseService: CoursesService, public dialog: MatDialog){
+  constructor(
+    private courseService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.courses$ = this.courseService.list()
-    .pipe(
-     catchError(error =>{
-      this.onError("Erro nessa porra")
-      return of([])
-     })
-    );
+      .pipe(
+        catchError(error => {
+          this.onError("Erro nessa porra")
+          return of([])
+        })
+      );
   }
 
-  onError(msg:string) {
+  onError(msg: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: msg
     });
   }
 
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route })
+  }
 
 
   ngOnInit(): void {
